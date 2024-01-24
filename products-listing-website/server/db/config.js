285c7ceb1e45feea057064fd4b549a9f43b109f4
paul <vickykirti2@gmail.com>
2024-01-24 14:59:07 +0530

    mongodb-compass-path-added

--- products-listing-website/server/db/config.js
+++ products-listing-website/server/db/config.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const mongoDB = async () => {
   try {
-    await mongoose.connect("");
+    await mongoose.connect("mongodb://127.0.0.1:27017/ecommerce");
     console.log("Database Connected");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
